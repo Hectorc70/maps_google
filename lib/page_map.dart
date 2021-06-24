f873756aa499596a30f0ee19ea353e958a04cb78
Hector Garcia Nieves <hecgarciann@gmail.com>
2021-06-24 12:39:26 -0500

    obtención de ubicación actual de usuario

--- lib/page_map.dart
+++ lib/page_map.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapSample extends StatefulWidget {
   @override
@@ -9,6 +11,14 @@ class MapSample extends StatefulWidget {
 }
 
 class MapSampleState extends State<MapSample> {
+  late Position _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
   Completer<GoogleMapController> _controller = Completer();
 
   static final CameraPosition _kGooglePlex = CameraPosition(
@@ -22,18 +32,68 @@ class MapSampleState extends State<MapSample> {
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
 
+  late GoogleMapController mapController;
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.normal,
-        myLocationEnabled: true,
-        myLocationButtonEnabled: true,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
+    return Stack(
+      children: [
+        Scaffold(
+          body: GoogleMap(
+            mapType: MapType.normal,
+            myLocationEnabled: true,
+            myLocationButtonEnabled: false,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              mapController = controller;
+            },
+          ),
+        ),
+        button_my_location(),
+      ],
     );
+  }
+
+  Widget button_my_location() {
+    return Container(
+        alignment: AlignmentDirectional.bottomCenter,
+        child: ClipOval(
+          child: Material(
+            color: Colors.orange.shade100, // button color
+            child: InkWell(
+              splashColor: Colors.orange, // inkwell color
+              child: SizedBox(
+                width: 56,
+                height: 56,
+                child: Icon(Icons.my_location),
+              ),
+              onTap: () async {
+                await _getCurrentLocation();
+              },
+            ),
+          ),
+        ));
+  }
+
+  _getCurrentLocation() async {
+    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+        .then((Position position) async {
+      setState(() {
+        // Store the position in the variable
+        _currentPosition = position;
+
+        print('Mi Posicion: $_currentPosition');
+
+        // Mueve a posicion actual
+        mapController.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(
+              target: LatLng(position.latitude, position.longitude),
+              zoom: 18.0,
+            ),
+          ),
+        );
+      });
+    });
   }
 }
